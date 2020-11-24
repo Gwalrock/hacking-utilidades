e84b61b1d3f1c463256fd721f4a5306238bbd62f
Gwalrock <44074230+Gwalrock@users.noreply.github.com>
2020-11-24 22:47:07 +0100

    Update simple_info.bat

--- Post-explotacion/simple_info.bat
+++ Post-explotacion/simple_info.bat
@@ -5,4 +5,7 @@ netstat -a  > netstat.txt
 ipconfig    > ipconfig.txt
 tasklist /V > tasklist_completo.txt
 netstat -an |find /i "listening" > open_ports.txt
+net user    > usuarios.txt
+gpresult /r > gpo_usuarios.txt
+query user  > usuarios_conectados.txt
 tracert www.google.com > tracert.txt
